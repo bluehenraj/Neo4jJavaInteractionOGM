1be70510a10d904848b8b8172c2644668d2d707a
Stephen Moss <smoss@udel.edu>
2020-11-10 20:09:24 -0500

    shared connection info

--- src/main/java/com/buddhadata/sandbox/neo4j/ogm/intro/Loader.java
+++ src/main/java/com/buddhadata/sandbox/neo4j/ogm/intro/Loader.java
@@ -33,9 +33,9 @@ public class Loader {
     private final SessionFactory sessionFactory;
 
     //  Configuration info for connecting to the Neo4J database
-    static private final String SERVER_URI = "bolt://52.201.184.95:33664";
+    static private final String SERVER_URI = "bolt://54.90.28.212:32813";
     static private final String SERVER_USERNAME = "neo4j";
-    static private final String SERVER_PASSWORD = "dockings-module-services";
+    static private final String SERVER_PASSWORD = "shields-letterhead-passivations";
 
 
     /**
